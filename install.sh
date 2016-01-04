b068a126c07ad909451748ebabe8e4a082c25755
x000032001 <s000032001@gmail.com>
2016-01-04 13:42:50 +0800

    fix

--- install.sh
+++ install.sh
@@ -3,30 +3,30 @@
 cd ~
 
 #install basic utils
-if [ "$(whoami)" == "root" ]; then
+if [ "$(whoami)" -eq "root" ]; then
 	apt-get update
 	apt-get install -y git tmux zsh curl
 fi
 
 #git
 which git
-$git = $?
-if [ $git != 0 ]; then
+git=$?
+if [ $git -ne 0 ]; then
 	echo "You need to install git."
 	exit
 fi
 
 #oh-my-zsh
 which zsh
-$zsh = $?
-if [ $zsh == 0 ]; then
+zsh=$?
+if [ $zsh -eq 0 ]; then
 	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 #tmux-powerline
 which tmux
-$tmux = $?
-if [ $tmux == 0 ]; then
+tmux=$?
+if [ $tmux -eq 0 ]; then
 	git clone https://github.com/erikw/tmux-powerline.git
 fi
 
@@ -36,6 +36,8 @@ git init
 git remote add origin https://github.com/xnum/myrc.git
 git fetch --all
 git reset --hard origin/master
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # install vim plugin
 vim +PluginInstall +qall
