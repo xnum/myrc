9b9f101f422569178b4ef59e6615c119d458dc62
葉家郡 <xnumtw@gmail.com>
2020-01-31 12:37:11 +0800

    Update install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -24,6 +24,7 @@ fi
 
 # zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 # autojump
 git clone git://github.com/wting/autojump.git /tmp/autojump
